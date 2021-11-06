fdd5911b647b7187427be55215ef47b97232a10c
Berk S <30241194+SaxDev@users.noreply.github.com>
2021-11-06 20:41:49 +0300

    Update remove.js

--- commands/gangs/remove.js
+++ commands/gangs/remove.js
@@ -11,7 +11,7 @@ module.exports = {
   execute(bot, message, args) {
     Guild.findOne({ guildID: message.guild.id }, async (err, guild) => {
       let user = guild.members.get(message.author.id)
-      let gang = guild.gangs.get(user.gang.name);
+      let gang = guild.gangs.get(user.gang.name)
       if (!user) {
           user = {
             id: message.author.id,
@@ -33,11 +33,9 @@ module.exports = {
         if (message.guild.ownerID != message.author.id && !message.member.permissions.has("ADMINISTRATOR") && user.gang.rank != "Owner") return message.error("You do not have permission to remove gangs. Only Administrators, Server Owner or Gang Owners can remove gangs.");
         gang = guild.gangs.get(args.join(" "));
         if (!gang) return message.error("This gang does not exist! Use the `g?list` command to see all gangs.");
+        if (message.author.id != gang.owner.id) return message.error("You are not the owner of this gang.)
       } else {
-        if (user.gang.rank != "Owner") return message.error("You do not have a gang to remove!");
-      }
-      if (user.gang.name != gang.name && message.guild.ownerID != message.author.id || !message.member.permissions.has("ADMINISTRATOR")) {
-        return message.error("You are not the owner of this Gang.")
+        if (user.gang.rank != "Owner") return message.error("You cannot remove a gang, you must be a Owner of the gang or a Server Admin to do it so.");
       }
 
       let confirm = await message.channel.send(`<:warning:724052384031965284> | Do you really wish to remove the **${gang.name}** gang? (yes/no)`);
